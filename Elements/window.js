8c843178d87fee7e1e8cebad2f2b07e4c3e1f2b4
Lucas <lucasjapa10@hotmail.com>
2022-07-09 15:33:59 -0300

    create window script done

--- Elements/window.js
+++ Elements/window.js
@@ -1,9 +1,38 @@
 function createDiv(className, location, idName) {
-    if(!idName) {
-        idName = className
-    }
+
     let div = document.createElement('div');
     div.setAttribute('class', className);
+    if(!idName) {
+        idName = className
+        div.setAttribute('id', idName);
+    }
     location.appendChild(div);
-
 }
+function createButton(location) {
+    let buttonOptions = [
+        'Minimize',
+        'Maximize',
+        'Close'
+    ];
+    let buttonId = [
+        'minimaze_calculator',
+        '',
+        'close_app_window_calculator'
+    ];
+    for(let i =0; i < buttonOptions.length; i++){
+        let button = document.createElement('button');
+        button.setAttribute('aria-label', buttonOptions[i]);
+        button.setAttibute('id', buttonId[i]);
+        location.appendChild(button);
+    };
+};
+function createWindow(windowName, appName) {
+    let desktop = document.querySelector('#dektop')
+    createDiv(`app_window ${appName}`,desktop, `app_window_${appName}`);
+    createDiv(`window ${appName}_window`, document.querySelector(`#app_window_${appName}`), 'window');
+    createDiv('title-bar', document.querySelector('#window'), `${appName}_tab`);
+    createDiv('title-bar-text', document.querySelector(`#${appName}_tab`));
+    document.querySelector('#title-bar-text').innerHTML = windowName;
+    createDiv('title-bar-controls', document.querySelector(`#${appName}_tab`));
+    createButton(document.querySelector('#title-bar-controls'));
+};
